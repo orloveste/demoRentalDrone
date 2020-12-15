1fbc98b9ebd8dc169f2cd6c488e4b566c4c2d021
orloveste <orlovevm@yahoo.com>
2020-12-15 11:17:51 +0200

    Initial commit

---
+++ src/main/java/rentalDrone/demoRentalDrone/DemoRentalDroneApplication.java
@@ -0,0 +1,13 @@
+package rentalDrone.demoRentalDrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRentalDroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRentalDroneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rentalDrone/demoRentalDrone/DemoRentalDroneApplicationTests.java
@@ -0,0 +1,13 @@
+package rentalDrone.demoRentalDrone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRentalDroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
